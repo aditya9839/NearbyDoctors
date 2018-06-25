bd17d6c39c20119514175d0da7b61ab9c623ff64
hacks2018 <adityaagarwal505@gmail.com>
2018-06-25 12:27:10 +0530

    hacks2018

--- sih2018/GIT Important stuffs/GoXP.care2/app/src/main/java/com/example/acer/goxpcare/MainActivity.java
+++ sih2018/GIT Important stuffs/GoXP.care2/app/src/main/java/com/example/acer/goxpcare/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.acer.goxpcare;
 
+import android.Manifest;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
     android.support.v4.app.FragmentTransaction fragmentTransaction;
     FragmentManager fragmentManager;
     DoctorList doctorList;
+
+    LocationManager l;
+    String str;
+    LocationListener locationListener;
+    String address = "abcd";
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -47,6 +69,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        CurrentLocation c = new CurrentLocation();
+//        c.getLocation();
+//        Toast.makeText(c, "add:"+c.address, Toast.LENGTH_SHORT).show();
+
+        getLocation();
 
         fragmentTransaction = getSupportFragmentManager().beginTransaction();
         doctorList = new DoctorList();
@@ -59,5 +86,67 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    public void getLocation() {
+        l = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        l.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("Location", location.toString());
+                double lat = location.getLatitude();
+                double longi = location.getLongitude();
+                Geocoder geocoder = new Geocoder(getApplicationContext());//GeoCoder will convert latitude and longitude into meaningful address
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(lat, longi, 1);//list of address of current location
+//                    str = addressList.get(0).getLocality();//get locality name (city)
+//                    str1 = addressList.get(0).getSubLocality();//get locality name (city)
+                    address = addressList.get(0).getAddressLine(0);
+//                        getCurrentWeather();
+                    Log.d("onLocationChanged: ", address);
+                    Toast.makeText(MainActivity.this, "add."+address, Toast.LENGTH_SHORT).show();
+
+//                    Toast.makeText(MainActivity.this, "" + str1+" "+str, Toast.LENGTH_SHORT).show();
+                    //    Toast.makeText(MainActivity.this, ""+address, Toast.LENGTH_SHORT).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+            }
+        };
+        if (Build.VERSION.SDK_INT < 23) {
+//            l.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                l.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                l.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
     }
 }
