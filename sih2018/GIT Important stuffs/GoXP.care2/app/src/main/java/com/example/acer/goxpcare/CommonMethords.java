7ca20d63a7822069ec1fcc412489384c3a37d8f9
hacks2018 <adityaagarwal505@gmail.com>
2018-06-29 13:00:53 +0530

    hacks2018

---
+++ sih2018/GIT Important stuffs/GoXP.care2/app/src/main/java/com/example/acer/goxpcare/CommonMethords.java
@@ -0,0 +1,8 @@
+package com.example.acer.goxpcare;
+
+/**
+ * Created by acer on 29-Jun-18.
+ */
+
+public class CommonMethords {
+}
---
+++ sih2018/GIT Important stuffs/GoXP.care2/app/src/main/java/com/example/acer/goxpcare/LoginSignUpPage.java
@@ -0,0 +1,58 @@
+package com.example.acer.goxpcare;
+
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginSignUpPage extends FragmentActivity {
+
+    android.support.v4.app.FragmentTransaction transaction;
+    Login login;
+    SignUpp signUpp;
+    Button mlogin,msignup;
+    int count = 0;
+    android.support.v4.app.FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sign_up_page);
+
+
+        fragmentManager = getSupportFragmentManager();
+
+        mlogin = findViewById(R.id.login);
+        msignup = findViewById(R.id.signup);
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login = new Login();
+//
+                transaction = fragmentManager.beginTransaction();
+
+                transaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
+//                use addToBackStack to go
+                transaction.replace(R.id.loginn, login).addToBackStack(null).commit();
+            }
+        });
+
+        msignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUpp = new SignUpp();
+//
+                transaction = fragmentManager.beginTransaction();
+
+                transaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
+                transaction.replace(R.id.loginn, signUpp).addToBackStack(null).commit();
+            }
+        });
+    }
+}
